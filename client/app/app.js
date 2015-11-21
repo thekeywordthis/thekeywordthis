4a0748bf0dbaa929682c8a0ada9413c25e686c45
Marc Reicher <msreicher@gmail.com>
2015-11-20 22:12:04 -0800

    User input is sent to server and written to a new file

--- client/app/app.js
+++ client/app/app.js
@@ -10,6 +10,21 @@ angular.module('thekeywordthis', ['ui.router'])
     })
 })
 
-.controller('mainController', function($scope) {
+.controller('mainController', function($scope, $http) {
+  $scope.code = {}
 
+  $scope.submitCode = function(code) {
+    console.log(JSON.stringify(code));
+    return $http({
+      method: 'POST',
+      url: '/usercode',
+      headers: {
+        "Content-Type": "application/JSON"
+      },
+      data: {code: code}
+    }).then(function(res) {
+      console.log(res.data);
+      $scope.code = res.data;
+    })
+  }
 })
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser  = require('body-parser');
 var http = require('http');
+var fs = require('fs');
 // var request = require('request');
 
 var app = express();
@@ -15,10 +16,19 @@ app.use('/', function(req, res, next) {
 })
 app.use(express.static(__dirname + '/../client'));
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/../client/assets/index.html'))
+})
+
+app.post('/usercode', function(req, res) {
+  // app.codeBlock = JSON.stringify(req.body.code);
+  // console.log(app.codeBlock);
+  fs.writeFileSync('./client/testCodeBlock.js', req.body.code);
+  res.send(JSON.stringify(req.body.code));
 })
 
 app.listen(3000);
 
 module.exports = app;
+
+// app.codeBlock;
